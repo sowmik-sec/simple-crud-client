95daf963779a6540a307873288a8cbe334d6f57e
sowmik-sec <sowmik.sec@gmail.com>
2024-07-16 11:17:35 +0600

    show multiple user in the ui

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,13 @@ function App() {
       body: JSON.stringify(user),
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          alert("User added successfully");
+          form.reset();
+        }
+      });
   };
   return (
     <>
